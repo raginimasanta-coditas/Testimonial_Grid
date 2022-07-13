44c125e65f37c858ea2bfb5e3b21c58d79eb9379
raginimasanta-coditas <ragini.masanta@coditas.com>
2022-07-13 15:49:34 +0530

    css updated

--- testimonial_grid/src/App.js
+++ testimonial_grid/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Template from "./component/template";
+import Flex from "./component/flex";
 
 function App() {
   return (
     <div className="App">
       <Template />
+      {/* <Flex /> */}
     </div>
   );
 }
--- testimonial_grid/src/component/template.js
+++ testimonial_grid/src/component/template.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./template.styles.scss";
-import image from "../assets/image-daniel.jpg";
+import Daniel from "../assets/image-daniel.jpg";
+import Jeanette from "../assets/image-jeanette.jpg";
+import Kira from "../assets/image-kira.jpg";
+import Patrick from "../assets/image-patrick.jpg";
+import Jonathan from "../assets/image-jonathan.jpg";
 
 class Template extends React.Component {
   constructor(props) {
@@ -16,7 +20,6 @@ class Template extends React.Component {
         <div class="grid-container">
           <div
             style={{
-              boxSizing: "content-box",
               backgroundColor: "hsl(263, 55%, 52%)",
               textAlign: "left",
               display: "flex",
@@ -26,11 +29,8 @@ class Template extends React.Component {
           >
             <div>
               <img
-                src={image}
+                src={Daniel}
                 style={{
-                  borderRadius: "30px",
-                  width: "33px",
-                  verticalAlign: "top",
                   border: "1.5px solid hsl(0, 0%, 100%)",
                 }}
               ></img>
@@ -39,12 +39,12 @@ class Template extends React.Component {
               <div className="name">Daniel Clifford </div>
               <div className="designation"> Verified Graduate</div>
             </div>
-            <div style={{ boxSizing: "content-box" }} className="first-para">
+            <div style={{}} className="first-para">
               I received a job offer mid-course, and the subjects I learned were
               current, if not more so, in the company I joined. I honestly feel
               I got every penny’s worth.
             </div>
-            <div style={{ boxSizing: "content-box" }} className="second-para">
+            <div style={{}} className="second-para">
               “ I was an EMT for many years before I joined the bootcamp. I’ve
               been looking to make a transition and have heard some people who
               had an amazing experience here. I signed up for the free intro
@@ -65,11 +65,8 @@ class Template extends React.Component {
           >
             <div>
               <img
-                src={image}
+                src={Jonathan}
                 style={{
-                  borderRadius: "30px",
-                  width: "33px",
-                  verticalAlign: "top",
                   border: "1.5px solid hsl(0, 0%, 100%)",
                 }}
               ></img>
@@ -78,10 +75,10 @@ class Template extends React.Component {
               <div className="name">Jonathan Walters</div>
               <div className="designation"> Verified Graduate</div>
             </div>
-            <div style={{ boxSizing: "content-box" }} className="first-para">
+            <div style={{}} className="first-para">
               The team was very supportive and kept me motivated
             </div>
-            <div style={{ boxSizing: "content-box" }} className="second-para">
+            <div style={{}} className="second-para">
               “ I started as a total newbie with virtually no coding skills. I
               now work as a mobile engineer for a big company. This was one of
               the best investments I’ve made in myself. ”
@@ -98,11 +95,8 @@ class Template extends React.Component {
           >
             <div>
               <img
-                src={image}
+                src={Kira}
                 style={{
-                  borderRadius: "30px",
-                  width: "33px",
-                  verticalAlign: "top",
                   border: "1.5px solid hsl(0, 0%, 100%)",
                 }}
               ></img>
@@ -115,14 +109,11 @@ class Template extends React.Component {
                 Verified Graduate
               </div>
             </div>
-            <div
-              style={{ boxSizing: "content-box", color: "hsl(219, 29%, 14%)" }}
-              className="first-para"
-            >
+            <div style={{ color: "hsl(219, 29%, 14%)" }} className="first-para">
               Such a life-changing experience. Highly recommended!
             </div>
             <div
-              style={{ boxSizing: "content-box", color: "hsl(219, 29%, 14%)" }}
+              style={{ color: "hsl(219, 29%, 14%)" }}
               className="second-para"
             >
               “ Before joining the bootcamp, I’ve never written a line of code.
@@ -150,18 +141,15 @@ class Template extends React.Component {
           >
             <div>
               <img
-                src={image}
+                src={Jeanette}
                 style={{
-                  borderRadius: "30px",
-                  width: "33px",
-                  verticalAlign: "top",
                   border: "1.5px solid hsl(0, 0%, 100%)",
                 }}
               ></img>
             </div>
-            <div style={{ width: "200px" }}>
+            <div>
               <div className="name" style={{ color: "black" }}>
-                Jeanette Harmon{" "}
+                Jeanette Harmon
               </div>
               <div
                 className="designation"
@@ -170,14 +158,11 @@ class Template extends React.Component {
                 Verified Graduate
               </div>
             </div>
-            <div
-              style={{ boxSizing: "content-box", color: "hsl(219, 29%, 14%)" }}
-              className="first-para"
-            >
+            <div style={{ color: "hsl(219, 29%, 14%)" }} className="first-para">
               An overall wonderful and rewarding experience
             </div>
             <div
-              style={{ boxSizing: "content-box", color: "hsl(219, 29%, 14%)" }}
+              style={{ color: "hsl(219, 29%, 14%)" }}
               className="second-para"
             >
               “ Thank you for the wonderful experience! I now have a job I
@@ -196,25 +181,22 @@ class Template extends React.Component {
           >
             <div>
               <img
-                src={image}
+                src={Patrick}
                 style={{
-                  borderRadius: "30px",
-                  width: "33px",
-                  verticalAlign: "top",
-                  border: "1.5px solid hsl(0, 0%, 100%)",
+                  border: "1.5px solid black",
                 }}
               ></img>
             </div>
-            <div style={{ width: "200px" }}>
+            <div>
               <div className="name">Patrick Abrams</div>
               <div className="designation"> Verified Graduate</div>
             </div>
-            <div style={{ boxSizing: "content-box" }} className="first-para">
+            <div className="first-para">
               Awesome teaching support from TAs who did the bootcamp themselves.
               Getting guidance from them and learning from their experiences was
               easy.
             </div>
-            <div style={{ boxSizing: "content-box" }} className="second-para">
+            <div style={{}} className="second-para">
               “ The staff seem genuinely concerned about my progress which I
               find really refreshing. The program gave me the confidence
               necessary to be able to go out in the world and present myself as
